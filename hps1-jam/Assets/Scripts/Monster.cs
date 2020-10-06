53615da481449e83a6dbd473624199405e02391f
cdom <dominguerilla@gmail.com>
2020-10-06 18:45:56 -0400

    Install Bolt, add wandering monster

---
+++ hps1-jam/Assets/Scripts/Monster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Monster : MonoBehaviour
+{
+    public GameObject[] huntingGrounds;
+    public float huntingGroundRadius = 5f;
+
+    NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void GoToRandomHuntingGround()
+    {
+        Vector3 huntingGroundPosition = GetRandomHuntingGroundPosition();
+        agent.SetDestination(huntingGroundPosition);
+    }
+
+    public bool isAtDestination()
+    {
+        return agent.remainingDistance < 1f;
+    }
+
+    Vector3 GetRandomHuntingGroundPosition()
+    {
+        GameObject randomHuntingGround = GetRandomHuntingGround();
+        float randX = Random.Range(0.5f, huntingGroundRadius);
+        float randZ = Random.Range(0.5f, huntingGroundRadius);
+
+        Vector3 randomOffset = new Vector3(randX, 0, randZ);
+        return randomHuntingGround.transform.position + randomOffset;
+    }
+
+    GameObject GetRandomHuntingGround()
+    {
+        return huntingGrounds[Random.Range(0, huntingGrounds.Length)];
+    }
+}
