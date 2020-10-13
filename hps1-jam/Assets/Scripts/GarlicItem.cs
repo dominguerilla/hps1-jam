6339b1e25256cf34c487a0ab830f32568dc1199c
cdom <dominguerilla@gmail.com>
2020-10-13 16:35:09 -0400

    Emit garlic particles on impact

--- hps1-jam/Assets/Scripts/GarlicItem.cs
+++ hps1-jam/Assets/Scripts/GarlicItem.cs
@@ -8,6 +8,9 @@ public class GarlicItem : ItemComponent
     public float throwForce = 25f;
     public float aimForDistance = 1f;
 
+    [SerializeField] ParticleSystem garlicParticles;
+
+    bool isBeingThrown = false;
 
     public override void Use()
     {
@@ -21,10 +24,36 @@ public class GarlicItem : ItemComponent
         Vector3 throwVector = camForward * throwForce;
 
         this.rb.AddForce(throwVector);
+        isBeingThrown = true;
     }
 
     Vector3 GetCameraForward()
     {
         return (Camera.main.transform.parent.forward + Camera.main.transform.forward).normalized;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isBeingThrown)
+        {
+            BreakApart();
+        }
+        else
+        {
+            isBeingThrown = false;
+        }
+    }
+
+    void BreakApart()
+    {
+        if (this.garlicParticles)
+        {
+            this.garlicParticles.Play();
+        }
+        else
+        {
+            Debug.LogWarning("No particles set!");
+        }
+        isBeingThrown = false;
     }
 }
